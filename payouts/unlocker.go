caaebc8e279887be10de71adac80609e89bc3353
happyxpie <happyxpie@test>
2018-03-13 12:41:43 -0400

    change byzantiumHeight & revert donation address

--- payouts/unlocker.go
+++ payouts/unlocker.go
@@ -29,14 +29,14 @@ type UnlockerConfig struct {
 }
 
 const minDepth = 16
-const byzantiumHardForkHeight = 4370000
+const byzantiumHardForkHeight = 0
 
 var homesteadReward = math.MustParseBig256("5000000000000000000")
 var byzantiumReward = math.MustParseBig256("2000000000000000000")
 
 // Donate 10% from pool fees to developers
 const donationFee = 10.0
-const donationAccount = "0xYourAddress"
+const donationAccount = "0xb85150eb365e7df0941f0cf08235f987ba91506a"
 
 type BlockUnlocker struct {
 	config   *UnlockerConfig
